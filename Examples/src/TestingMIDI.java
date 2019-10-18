38b3a8e3ef87d0eef51caf09b8af976c10355779
chhawkins <chhawkins@ksu.edu>
2019-10-18 11:25:46 -0500

    Played around more with midiChannel

--- Examples/src/TestingMIDI.java
+++ Examples/src/TestingMIDI.java
@@ -23,9 +23,27 @@ public class TestingMIDI {
         Instrument[] instruments = synth.getDefaultSoundbank().getInstruments();
         synth.loadInstrument(instruments[0]);
         MidiChannel[] midiChannels = synth.getChannels();
+        midiChannels[4].programChange(10); // sets the instrument
         //megins playing here.
         midiChannels[4].noteOn(46, 100);
+        Thread.sleep(50);
+        midiChannels[4].noteOn(50, 100);
+        Thread.sleep(50);
+        midiChannels[4].noteOn(53, 100);
+        Thread.sleep(50);
+        midiChannels[4].noteOn(57, 100);
         Thread.sleep(5000);
         midiChannels[4].noteOff(46, 60);
+        midiChannels[4].noteOff(50, 60);
+        midiChannels[4].noteOff(53, 60);
+        midiChannels[4].noteOff(57, 60);
+
+        for (int i = 20; i < 80; i++){
+            midiChannels[4].noteOn(i, 100);
+            Thread.sleep(50);
+        }
+
+        midiChannels[4].allSoundOff();
+
     }
 }
