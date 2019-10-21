72fdf0d1c2103f0c0bb2f4a0a0ddcd56590bc094
chhawkins <chhawkins@ksu.edu>
2019-10-21 12:48:11 -0500

    added note definitions for the number keys and top row of letters

--- Core/src/Piano.java
+++ Core/src/Piano.java
@@ -1,7 +1,38 @@
-import javax.sound.midi.*;
 import java.awt.event.KeyEvent;
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 public class Piano extends InstrumentHandler{
+    public Dictionary<Integer, Integer> notes = new Hashtable<Integer, Integer>();
+
+    {
+        //top row of letters
+        notes.put(81, 60);
+        notes.put(87, 61);
+        notes.put(69, 62);
+        notes.put(82, 63);
+        notes.put(84, 64);
+        notes.put(89, 65);
+        notes.put(85, 66);
+        notes.put(73, 67);
+        notes.put(79, 68);
+        notes.put(80, 69);
+        notes.put(91, 70);
+        notes.put(93, 71);
+        //numbers
+        notes.put(49, 72);
+        notes.put(50, 73);
+        notes.put(51, 74);
+        notes.put(52, 75);
+        notes.put(53, 76);
+        notes.put(54, 77);
+        notes.put(55, 78);
+        notes.put(56, 79);
+        notes.put(57, 80);
+        notes.put(48, 81);
+        notes.put(45, 82);
+        notes.put(61, 83);
+    }
 
     public Piano(SoundSynthesizer synth) {
         super(synth);
@@ -11,14 +42,14 @@ public class Piano extends InstrumentHandler{
     @Override
     public void keyPressed(KeyEvent keyEvent) {
         int keyCode = keyEvent.getKeyCode();
-        Note note = new Note(keyCode, 100, true);
+        Note note = new Note(notes.get(keyCode), 100, true);
         synth.playNote(this, note);
     }
 
     @Override
     public void keyReleased(KeyEvent keyEvent) {
         int keyCode = keyEvent.getKeyCode();
-        Note note = new Note(keyCode, 100, false);
+        Note note = new Note(notes.get(keyCode), 100, false);
         synth.playNote(this, note);
     }
 
